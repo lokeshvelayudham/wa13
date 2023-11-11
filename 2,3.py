7b7949d299b1c889375856a8d56363051e228af8
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2023-11-11 16:38:40 +0530

    updzate

--- 2,3.py
+++ 2,3.py
@@ -2,19 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Hypotheses
-p_cherry_given_h = [1, 0.75, 0.5, 0.25, 0]
-p_lime_given_h = [0, 0.25, 0.5, 0.75, 1]
+cherry_hypo = [1, 0.75, 0.5, 0.25, 0]
+lime_hypo = [0, 0.25, 0.5, 0.75, 1]
 
 # Prior probabilities for each hypothesis
 p_h = [1/5]*5
 
 def generate_data(hypothesis, N=100):
-    return np.random.choice(['cherry', 'lime'], N, p=[p_cherry_given_h[hypothesis], p_lime_given_h[hypothesis]])
+    return np.random.choice(['cherry', 'lime'], N, p=[cherry_hypo[hypothesis], lime_hypo[hypothesis]])
 
 def compute_posterior(data):
     likelihoods = []
     for h in range(5):
-        likelihood = np.prod([p_cherry_given_h[h] if d == 'cherry' else p_lime_given_h[h] for d in data])
+        likelihood = np.prod([cherry_hypo[h] if d == 'cherry' else lime_hypo[h] for d in data])
         likelihoods.append(likelihood)
     
     evidence = sum(np.array(likelihoods) * np.array(p_h))
@@ -23,7 +23,7 @@ def compute_posterior(data):
 
 def predict_next_candy(data):
     posteriors = compute_posterior(data)
-    return sum(posteriors[i] * p_lime_given_h[i] for i in range(5))
+    return sum(posteriors[i] * lime_hypo[i] for i in range(5))
 
 # For hypothesis h3 and h4:
 datasets = {f"h{i}": generate_data(i-1) for i in [3, 4]}
