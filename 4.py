82b3d08f6cd71a4d9fcf519efa37d68afe817c8b
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2023-11-12 22:04:13 +0530

    d

--- 4.py
+++ 4.py
@@ -1,176 +1,68 @@
-from functools import reduce
-from random import randint
-import math
-import collections
-import matplotlib.pyplot as plt
 import numpy as np
-
-# distributions of each of the hypotheses
-h1 = { 'cherry': 1.0, 'lime': 0.0 }
-h2 = { 'cherry': 0.75, 'lime': 0.25 }
-h3 = { 'cherry': 0.5, 'lime': 0.5 }
-h4 = { 'cherry': 0.25, 'lime': 0.75 }
-h5 = { 'cherry': 0.0, 'lime': 1.0 }
-
-# Original ditribution of the hypotheses from the textbook
-p_hypotheses = [0.1, 0.2, 0.4, 0.2, 0.1]
-
-def p_cond(D, h):
-    """
-    Return the conditional probability of a dataset D given a hypothesis h.
-
-    For this problem, the elements of D are statistically independent, so we can
-    just take the product of the conditional probabilities of each element in D.
-    """
-    return reduce((lambda acc, el: acc * h[el]), D, 1)
-
-def compute_MAP_hypothesis(D, hypotheses, p_vector):
-    """
-    Return the MAP hypothesis for the dataset D.
-    p_vector is the vector of probabilities of each hypothesis.
-    For the purposes of this assignment, only consider h3 and h4.
-    """
-    MAP, MAP_max = None, None
-    for i in range(3,5):
-        p_hi = p_vector[i]
-        _p_cond = p_cond(D, hypotheses[i])
-        if p_hi == 0 or _p_cond == 0:
-            # Depending on the dataset, probabilities can sometimes go to 0,
-            # which breaks the log method. Don't let the happen.
-            continue
-        # math.log accepts a base as the second argument
-        print("made it here")
-        MAP_val = -math.log(_p_cond, 2) - math.log(p_hi, 2)
-        if MAP_max is None or MAP_max < MAP_val:
-            MAP = hypotheses[i] # this can be modified to return just the index, if need be.
-    return MAP
-
-def compute_ML_hypothesis(D, hypotheses):
-    """
-    Return the ML hypothesis for the dataset D.
-    The ML hypothesis is the hypothesis which maximizes P(D|hi).
-    For the puposes of this assignment, only consider h3 and h4.
-    """
-    ML, ML_max = None, None
-    for i in range(3,5):
-        ML_val = p_cond(D, hypotheses[i])
-        if ML_max is None or ML_max < ML_val:
-            ML = hypotheses[i]
-    return ML
-
-#Hypothesis 3: 50% Cherry, 50% Lime
-#Hypothesis 4: 25% Cherry, 75% Lime
-
-h3_data = []
-h4_data = []
-
-for i in range(1,101):
-    candidate = randint(0,100)
-    if candidate < 50:
-        h3_data.append("cherry")
-    else:
-        h3_data.append("lime")
-    candidate = randint(0,100)
-    if candidate < 25:
-        h4_data.append("cherry")
-    else:
-        h4_data.append("lime")
-
-# Need one matrix for the h3 dataset and another for the h4 dataset
-p_h3_matrix = [p_hypotheses]
-p_h4_matrix = [p_hypotheses]
-
-hypotheses = [h1, h2, h3, h4, h5] # for mapping and indexing
-
-h3_part_i, h3_part_ii, h3_part_iii, h3_part_iv = [], [], [], []
-h4_part_i, h4_part_ii, h4_part_iii, h4_part_iv = [], [], [], []
-for n in range(1,101):
-    # Compute the P(hi|D[0:n]) for h1..h5. This is part i
-    prev_h3_row = p_h3_matrix[n - 1]
-    next_h3_row = []
-    for i in range(0,5):
-        p_hi = prev_h3_row[i]
-        denominator = 0.0
-        for k in range(0,5): # I hate everything
-            denominator += p_cond(h3_data[0:n], hypotheses[k]) * prev_h3_row[k]
-        p_hi_given_d = (p_cond(h3_data[0:n], hypotheses[i]) * p_hi) / denominator
-        next_h3_row.append(p_hi_given_d)
-    p_h3_matrix.append(next_h3_row)
-    
-    prev_h4_row = p_h4_matrix[n - 1]
-    next_h4_row = []
-    for i in range(0,5):
-        denominator = 0.0
-        for k in range(0,5):
-            denominator += p_cond(h4_data[0:n], hypotheses[k]) * prev_h4_row[k]
-        p_hi = prev_h4_row[i]
-        p_hi_given_d = (p_cond(h4_data[0:n], hypotheses[i]) * p_hi) / denominator
-        next_h4_row.append(p_hi_given_d)
-    p_h4_matrix.append(next_h4_row)
-
-# more loops! Monkeys and Wabbits Loop de loop
-for n in range(0,100):
-    # Part ii
-    # P(Dn+1 = lime | d1,...,dn) = sum i = 1 to 5 p(dn+1 = lime | hi) * p(hi|d)
-    # That last part of the product is what's in our matrices.
-    
-    # h3 case:
-    h3_sum = 0
-    h3_row = p_h3_matrix[n]
-    for i in range(0,5):
-        p_hi = h3_row[i] # this should be a probability
-        h3_sum += p_hi * hypotheses[i]['lime']
-    h3_part_ii.append(h3_sum)
-    
-    # h4 case:
-    h4_sum = 0
-    h4_row = p_h4_matrix[n]
-    for i in range(0,5):
-        p_hi = h4_row[i]
-        h4_sum += p_hi * hypotheses[i]['lime']
-    h4_part_ii.append(h4_sum)
-
-for n in range(0,100):
-    # part iii
-    
-    # h3 case:
-    print (p_h3_matrix[n])
-    h3_MAP = compute_MAP_hypothesis(h3_data[0:n], hypotheses, p_h3_matrix[n])
-    # Now that we have a MAP hypothesis, we can compute P(d_n+1 = 'lime' | hMAP)
-    h3_part_iii.append(h3_MAP['lime'])
-    
-    # h4 case:
-    h4_MAP = compute_MAP_hypothesis(h4_data[0:n], hypotheses, p_h4_matrix[n]) 
-    h4_part_iii.append(h4_MAP['lime'])
-
-for n in range(0,100):
-    # part iv
-
-    # h3 case:
-    h3_ML = compute_ML_hypothesis(h3_data[0:n], hypotheses)
-    # Now that we have a ML hypothesis, we can compute P(d_n+1 = 'lime' | hML)
-    h3_part_iv.append(h3_ML['lime'])
-
-    # h4 case:
-    h4_ML = compute_ML_hypothesis(h4_data[0:n], hypotheses)
-    h4_part_iv.append(h4_ML['lime'])
-
-
-# Graph for i for h3:
-# plt.plot(range(0,100), i)
-
-# Graph for ii, iii, iv for h3:
-plt.plot(range(0, 100), h3_part_ii, label = 'h3_part ii - Post')
-plt.plot(range(0,100),h3_part_iii, label = 'h3_part iii- map')
-plt.plot(range(0,100),h3_part_iv, label = 'h3_part iv - ml')
+import matplotlib.pyplot as plt
+# Define the hypotheses and their probabilities
+hypotheses = ["h1", "h2", "h3", "h4", "h5"]
+prior = [0.1, 0.2, 0.4, 0.2, 0.1]
+likelihood = [1, 0.75, 0.5, 0.25, 0]
+# Define a function to generate a dataset of size n for a given hypothesis
+def generate_data(hypothesis, n):
+    if hypothesis == "h1":
+        return [1] * n
+    return [1 if np.random.rand() < likelihood[hypotheses.index(hypothesis)]
+else 0 for _ in range(n)]
+# Define a function to compute the posterior probabilities for each hypothesis
+given a dataset
+def compute_posterior(data):
+posteriors = [prior[i] * (likelihood[i] ** sum(data)) * ((1 -
+likelihood[i]) ** (len(data) - sum(data))) for i in range(len(hypotheses))]
+return [p / sum(posteriors) for p in posteriors]
+# Define a function to compute the probability of the next data point being
+lime given the dataset
+def compute_next_lime(data):
+posteriors = compute_posterior(data)
+return sum([posteriors[i] * likelihood[i] for i in
+range(len(hypotheses))])
+# Define a function to compute the maximum a posteriori (MAP) hypothesis given
+the dataset
+def compute_map(data):
+posteriors = compute_posterior(data)
+return hypotheses[posteriors.index(max(posteriors))]
+# Define a function to compute the maximum likelihood (ML) hypothesis given
+the dataset
+def compute_ml(data):
+likelihoods = [likelihood[i] ** sum(data) * ((1 - likelihood[i]) **
+(len(data) - sum(data))) for i in range(len(hypotheses))]
+return hypotheses[likelihoods.index(max(likelihoods))]
+# Generate the datasets for h3 and h4
+data_h3 = generate_data("h3", 100)
+data_h4 = generate_data("h4", 100)
+# Plot the graphs for P(DN+1=lime|hMAP) and P(DN+1=lime|hML) as N varies from
+1 to 100
+plt.figure(figsize=(12, 8))
+# Plot the graph for h3
+plt.subplot(2, 1, 1)
+plt.title("Data generated by h3")
+plt.xlabel("N")
+plt.ylabel("P(DN+1=lime|hMAP) or P(DN+1=lime|hML)")
+plt.ylim(0, 1)
+next_lime_map_h3 = [likelihood[hypotheses.index(compute_map(data_h3[:n]))] for
+n in range(1, 101)]
+next_lime_ml_h3 = [likelihood[hypotheses.index(compute_ml(data_h3[:n]))] for n
+in range(1, 101)]
+plt.plot(range(1, 101), next_lime_map_h3, label="P(DN+1=lime|hMAP)")
+plt.plot(range(1, 101), next_lime_ml_h3, label="P(DN+1=lime|hML)")
+plt.legend()
+# Plot the graph for h4
+plt.subplot(2, 1, 2)
+plt.title("Data generated by h4")
+plt.xlabel("N")
+plt.ylabel("P(DN+1=lime|hMAP) or P(DN+1=lime|hML)")
+plt.ylim(0, 1)
+next_lime_map_h4 = [likelihood[hypotheses.index(compute_map(data_h4[:n]))] for
+n in range(1, 101)]
+next_lime_ml_h4 = [likelihood[hypotheses.index(compute_ml(data_h4[:n]))] for n
+in range(1, 101)]
+plt.plot(range(1, 101), next_lime_map_h4, label="P(DN+1=lime|hMAP)")
+plt.plot(range(1, 101), next_lime_ml_h4, label="P(DN+1=lime|hML)")
 plt.legend()
 plt.show()
-
-# Graph for i for h4:
-# Graph for ii, iii, and iv for h4:
-plt.plot(range(0, 100), h4_part_ii, label = 'h4_part ii - Post')
-plt.plot(range(0,100),h4_part_iii, label = 'h4_part iii- map')
-plt.plot(range(0,100),h4_part_iv, label = 'h4_part iv- ml')
-plt.legend()
-plt.show()
-
--- sam7.py
+++ sam7.py
@@ -1,46 +1,95 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Step 2: Set Prior Probability and Define Likelihood Functions
-# Prior probability of h3
-prior_h3 = 0.9
+# Hypotheses
+cherry_hypo = [1, 0.75, 0.5, 0.25, 0]
+lime_hypo = [0, 0.25, 0.5, 0.75, 1]
 
-# Arbitrary likelihood functions for h3 and h4
-def likelihood_h3(N):
-    return np.exp(-0.1 * N)
+# Prior probabilities for each hypothesis
+priors = np.array([0.1, 0.2, 0.4, 0.2, 0.1])
 
-def likelihood_h4(N):
-    return np.exp(-0.05 * N)
 
-# Step 3: Initialize Lists for Probabilities
-# Initialize probabilities
-prob_map = []
-prob_ml = []
+def generate_data(hypothesis, N=100):
+    return np.random.choice(['cherry', 'lime'], N, p=[cherry_hypo[hypothesis], lime_hypo[hypothesis]])
 
-# Step 4: Calculate Probabilities for Each N
-# Iterate over N from 1 to 100
-for N in range(1, 101):
-    # Calculate likelihoods
-    lh_h3 = likelihood_h3(N)
-    lh_h4 = likelihood_h4(N)
 
-    # Calculate posterior probabilities
-    posterior_h3 = (lh_h3 * prior_h3) / ((lh_h3 * prior_h3) + (lh_h4 * (1 - prior_h3)))
-    posterior_h4 = 1 - posterior_h3
+def calculate_posterior(data):
+    likelihoods = []
+    for h in range(5):
+        likelihood = np.prod(
+            [cherry_hypo[h] if d == 'cherry' else lime_hypo[h] for d in data])
+        likelihoods.append(likelihood)
 
-    # Update prior for the next iteration
-    prior_h3 = posterior_h3
+    evidence = sum(np.array(likelihoods) * np.array(priors))
+    posteriors = (np.array(likelihoods) * np.array(priors)) / evidence
+    # print("old:", posteriors)
+    return posteriors
 
-    # Probability of observing lime given h_MAP and h_ML
-    prob_map.append(lh_h3 * posterior_h3 + lh_h4 * posterior_h4)
-    prob_ml.append(max(lh_h3, lh_h4))
 
-# Step 5: Plot the Results
-# Plot the results
-plt.plot(range(0, 100), prob_map, label='P(DN+1=lime|h_MAP)')
-plt.plot(range(0, 100), prob_ml, label='P(DN+1=lime|h_ML)')
-plt.xlabel('N')
-plt.ylabel('Probability')
-plt.legend()
-plt.title('Probability of Observing "lime" under Different Hypotheses')
-plt.show()
+def predict_next_candy(data):
+    posteriors = calculate_posterior(data)
+    return sum(posteriors[i] * lime_hypo[i] for i in range(5))
+
+
+# to plot the graph
+# For hypothesis h3 and h4:
+datasets = {f"h{i}": generate_data(i-1) for i in [3, 4]}
+
+def find_hmap_hml(posteriors, prior_h3=0.9):
+    # Adjust priors for h3 and h4
+    adjusted_priors = [0, 0, prior_h3, 1 - prior_h3, 0]
+
+    # hMAP: hypothesis with the highest posterior probability
+    normalized_posterior = [posteriors[i]/1 for i in range(5)]
+    hmapcal = [normalized_posterior[i]*adjusted_priors[i] for i in range(5)]
+    hmap = max(hmapcal)
+    # calculate the maximum value and then return the index of max
+    hmapind = hmapcal.index(hmap)
+
+    # print(posteriors)
+
+    # hML: among h3 and h4, the one with the higher adjusted posterior
+    hmlmax = max(normalized_posterior)
+
+   # calculate the maximum value and then return the index of max
+    hmlindex = normalized_posterior.index(hmlmax)
+
+    return hmapind, hmlindex
+
+
+# plot
+for hypothesis, data in datasets.items():
+    posteriors_over_time = []
+    predictions_over_time = []
+    predictions_hmap_over_time = []
+    predictions_hml_over_time = []
+
+    for N in range(0, 100):
+        current_data = data[:N]
+        posteriors = calculate_posterior(current_data)
+        posteriors_over_time.append(posteriors)
+        predictions_over_time.append(predict_next_candy(current_data))
+        hmap, hml = find_hmap_hml(posteriors)
+        predictions_hmap_over_time.append(lime_hypo[hmap])
+        predictions_hml_over_time.append(lime_hypo[hml])
+
+    # Plotting Posterior Probabilities and Predictions for Lime Candy
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    for i in range(5):
+        plt.plot(range(0, 100), [posterior[i]for posterior in posteriors_over_time], label=f'h{i+1}')
+    plt.xlabel('N')
+    plt.ylabel('P(hi|d)')
+    plt.legend()
+    plt.title(f'Posterior Probabilities for {hypothesis}')
+    plt.subplot(1, 2, 2)
+    plt.plot(range(0, 100), predictions_over_time, label='P(DN+1=lime|d)')
+    plt.plot(range(0, 100), predictions_hmap_over_time,label='P(DN+1=lime|hMAP)', linestyle='--')
+    plt.plot(range(0, 100), predictions_hml_over_time, label='P(DN+1=lime|hML)', linestyle='-.')
+    plt.xlabel('N')
+    plt.ylabel('Probability')
+    plt.title(f'Prediction for Lime Candy for {hypothesis}')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
